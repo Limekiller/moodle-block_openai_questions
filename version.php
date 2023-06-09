cea3dce4464a5bcb888c305267f07a93d9bdd754
Limekiller <me@bryceyoder.com>
2023-06-09 21:01:11 +0000

    Add version.php info

--- version.php
+++ version.php
@@ -26,4 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2023060900;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2020061500;        // Requires this Moodle version
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0.0';
 $plugin->component = 'block_openai_questions'; // Full name of the plugin (used for diagnostics)
